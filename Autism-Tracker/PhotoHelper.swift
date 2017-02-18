a9c82d4d68e77e0b429a4ea0bcacb42fd96f0c29
ewjc <ericswong7@gmail.com>
2017-02-17 23:36:59 -0800

    updated mainstory + added photo helper

---
+++ Autism-Tracker/PhotoHelper.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoHelper.swift
+//  Autism-Tracker
+//
+//  Created by Eric Wong on 2/17/17.
+//  Copyright © 2017 Tawpy. All rights reserved.
+//
+
+import UIKit
+
+typealias PhotoHelperCallBack = (UIImage?) -> Void
+
+class PhotoTakingHelper: NSObject {
+    
+
+}
