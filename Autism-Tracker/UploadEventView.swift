df5d9c347f31b4809b25fa758053b1adb2e8bf92
ewjc <ericswong7@gmail.com>
2017-02-17 22:53:28 -0800

    added new view

---
+++ Autism-Tracker/UploadEventView.swift
@@ -0,0 +1,20 @@
+//
+//  EventView.swift
+//  Autism-Tracker
+//
+//  Created by Eric Wong on 2/17/17.
+//  Copyright © 2017 Tawpy. All rights reserved.
+//
+
+import UIKit
+
+class UploadEventView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
