1dea90571de1d3b68b216e88d68df98bb6211899
Nicholas Swift <nicholas.w.swift@gmail.com>
2017-02-19 02:16:34 -0800

    neura

--- Autism-Tracker/AppDelegate.swift
+++ Autism-Tracker/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import NeuraSDK
 import Firebase
 
 @UIApplicationMain
@@ -19,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Firebase
         FIRApp.configure()
+        
+        // Neura
+        NeuraSDK.shared.appUID = "6313ad76be25b88a4c8afab56e15f3f24a13c4596f479da01e907d766f11bb1d"
+        NeuraSDK.shared.appSecret = "3cf5101e7e5c7a805d7a6965de59ebb07eade562ecf0ca556247205405ffa4c7"
         
         // UI Navigation Bar
         UIApplication.shared.statusBarStyle = .lightContent
--- Autism-Tracker/UpdateChildStatusView.swift
+++ Autism-Tracker/UpdateChildStatusView.swift
@@ -10,28 +10,92 @@ import UIKit
 import NeuraSDK
 
 
-class UpdateChildStatusView: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class UpdateChildStatusView: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
+    // MARK: - Instance Vars
+    var authToken: String = ""
     let eventArray = ["userArrivedHome", "userLeftHome", "userIsIdleAtHome", "userStartedWalking", "userStartedRunning", "userStartedSleeping", "userWokeUp", "dailySummary"]
     
+    // MARK: - Subviews
+    @IBOutlet weak var tableView: UITableView!
     
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for index in 0..<eventArray.count {
-            let newSubscription = NSubscription(eventName: eventArray[index],
-                                                identifier: "subscription unique identifier",
-                                                webhookId: "UpdateStatus")
-            NeuraSDK.shared.add(newSubscription) { result in
+        setupNeura()
+    }
+    
+}
+
+// MARK: - Neura Events
+extension UpdateChildStatusView {
+    
+    func setupNeura() {
+        setupNeuraAuth()
+        setupNeuraEvents()
+        setupNeuraGetEvents()
+    }
+    
+    func setupNeuraAuth() {
+        
+        // Authentication request
+        let authRequest = NeuraAuthenticationRequest(controller: self)
+        
+        // Authenticate.
+        NeuraSDK.shared.authenticate(with: authRequest) { result in
+            if result.success {
+                // Success. result.token available
+                self.authToken = result.token!
+            } else {
+                // Failed. An error description available on result.error
             }
         }
+    }
+    
+    func setupNeuraEvents() {
+        // Create indexes
+        for event in eventArray {
+            
+            // Create new subscription
+            let newSubscription = NSubscription(eventName: event, identifier: "\(event.hashValue)", webhookId: "UpdateStatus")
+            
+            // Add subscription
+            NeuraSDK.shared.add(newSubscription, callback: { (result: NeuraAddSubscriptionResult) in
+                print(result)
+            })
+        }
+    }
+    
+    func setupNeuraGetEvents() {
+        // Get the subscriptions list
+        NeuraSDK.shared.getSubscriptionsList() { result in
+            if result.success {
+                print(result.subscriptions)
+            }
+        }
+    }
+    
+}
 
+// MARK: - Table View
+extension UpdateChildStatusView {
+    
+    func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.reloadData()
-        
     }
+    
+}
+
+// MARK: - Table View Delegate
+extension UpdateChildStatusView: UITableViewDelegate {
+    
+}
+
+// MARK: - Table View Data Source
+extension UpdateChildStatusView: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -46,12 +110,9 @@ class UpdateChildStatusView: UIViewController, UITableViewDelegate, UITableViewD
         
         cell.statusLabel.text = ""
         cell.dateLabel.text = ""
-    
+        
         return cell
     }
-    
-    
-    
     
 }
 
