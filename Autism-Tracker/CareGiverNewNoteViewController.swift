88fba36d188a5fe0584567855e4a9d193cd42c1a
Alexander Dejeu <alexdejeu@gmail.com>
2017-02-18 16:57:05 -0800

    Added the care giver add note UI

--- Autism-Tracker/CareGiverNewNoteViewController.swift
+++ Autism-Tracker/CareGiverNewNoteViewController.swift
@@ -9,21 +9,42 @@
 import UIKit
 
 class CareGiverNewNoteViewController: UIViewController {
-    
-    // MARK: - Instance Vars
-    
-    // MARK: - Subviews
-    @IBOutlet weak var cancelBarButtonItem: UIBarButtonItem!
-    
-    // MARK: - Subview Actions
-    @IBAction func cancelBarButtonAction(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-        dismiss(animated: true, completion: nil)
-    }
-    
-    // MARK: - View Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+  //MARK: - IBOutlets
+  @IBOutlet weak var triggerTextField : UITextField!
+  @IBOutlet weak var resolutionTextField : UITextField!
+  @IBOutlet weak var notesTextArea : UITextView!
+  @IBOutlet weak var happyButton : UIButton!
+  @IBOutlet weak var shockedButton : UIButton!
+  @IBOutlet weak var neutralButton : UIButton!
+  @IBOutlet weak var angryButton : UIButton!
+  @IBOutlet weak var sadButton : UIButton!
+  
+  @IBOutlet weak var activityLevel : customDragView!
+  @IBOutlet weak var selfHarmLevel : customDragView!
+  @IBOutlet weak var stressLevel : customDragView!
+  
+  @IBOutlet weak var saveButton : UIButton!
+  
+  @IBAction func saveNote(sender: UIButton){
+    print("SAVE THE NOTE")
+    dismiss(animated: true, completion: nil)
+  }
+  
+  
+  // MARK: - Instance Vars
+  
+  // MARK: - Subviews
+  @IBOutlet weak var cancelBarButtonItem: UIBarButtonItem!
+  
+  // MARK: - Subview Actions
+  @IBAction func cancelBarButtonAction(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+    dismiss(animated: true, completion: nil)
+  }
+  
+  // MARK: - View Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
 }
---
+++ Autism-Tracker/Custom Elements/customDragView.swift
@@ -0,0 +1,22 @@
+//
+//  customDragView.swift
+//  Autism-Tracker
+//
+//  Created by Alexander Dejeu on 2/18/17.
+//  Copyright © 2017 Tawpy. All rights reserved.
+//
+
+import UIKit
+
+class customDragView: UIView {
+  
+  init(frame: CGRect, title: String, maxValue : Int){
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+//    fatalError("init(coder:) has not been implemented")
+    super.init(coder: aDecoder)
+  }
+
+}
